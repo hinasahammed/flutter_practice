2170c05513e20afa5b18891a6da1e228e7d5b8ff
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-02 20:20:18 +0530

    hidden drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/practice/drawer/drawer_practice.dart';
+import 'package:flutter_practice/practice/drawer/drawer_with_packages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const DrawerPractice(),
+      home: const DrawerWithPackages(),
     );
   }
 }
---
+++ lib/practice/drawer/drawer_with_packages.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+import 'package:hidden_drawer_menu/model/item_hidden_menu.dart';
+import 'package:hidden_drawer_menu/model/screen_hidden_drawer.dart';
+
+class DrawerWithPackages extends StatefulWidget {
+  const DrawerWithPackages({super.key});
+
+  @override
+  State<DrawerWithPackages> createState() => _DrawerWithPackagesState();
+}
+
+class _DrawerWithPackagesState extends State<DrawerWithPackages> {
+  List<ScreenHiddenDrawer> itens = [];
+
+  @override
+  void initState() {
+    itens.add(ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Screen 1",
+          baseStyle:
+              TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 28.0),
+          colorLineSelected: Colors.teal,
+          selectedStyle: const TextStyle(
+            color: Colors.red,
+          ),
+        ),
+        Container(
+          color: Colors.red,
+        )));
+
+    itens.add(ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name: "Screen 2",
+          baseStyle:
+              TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 28.0),
+          colorLineSelected: Colors.orange,
+          selectedStyle: const TextStyle(
+            color: Colors.red,
+          ),
+        ),
+        Container(
+          color: Colors.green,
+        )));
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HiddenDrawerMenu(
+      backgroundColorMenu: Colors.blueGrey,
+      backgroundColorAppBar: Colors.cyan,
+      screens: itens,
+    );
+  }
+}
