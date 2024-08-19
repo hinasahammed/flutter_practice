5ecfbb7bce5b815343ce80f7ba2ce5a333b8dd2d
ahammed hinas <82311707+hinasahammed@users.noreply.github.com>
2024-08-19 10:30:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/practice/calculator/calculator_practice.dart';
+import 'package:flutter_practice/practice/drawer/advanced_drawer_custom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const CalculatorPractice(),
+      home: const AdvancedDrawerCustom(),
     );
   }
 }
