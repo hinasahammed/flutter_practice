982d70df3703905fc99b982540fdd91f82c824a4
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-09-11 22:23:01 +0530

    removed geolocatr

--- lib/practice/apiCalling/weather_api.dart
+++ lib/practice/apiCalling/weather_api.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
 class WeatherApi extends StatefulWidget {
   const WeatherApi({super.key});
 
@@ -33,28 +31,4 @@ class _WeatherApiState extends State<WeatherApi> {
       ),
     );
   }
-
-  // Future<Position> _determinePosition() async {
-  //   bool serviceEnabled;
-  //   LocationPermission permission;
-
-  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  //   if (!serviceEnabled) {
-  //     return Future.error('Location services are disabled.');
-  //   }
-
-  //   permission = await Geolocator.checkPermission();
-  //   if (permission == LocationPermission.denied) {
-  //     permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied) {
-  //       return Future.error('Location permissions are denied');
-  //     }
-  //   }
-
-  //   if (permission == LocationPermission.deniedForever) {
-  //     return Future.error(
-  //         'Location permissions are permanently denied, we cannot request permissions.');
-  //   }
-  //   return await Geolocator.getCurrentPosition();
-  // }
 }
