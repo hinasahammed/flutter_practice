722789945d836079d54e9617dfcd9b114a6da5d6
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-09-11 23:12:17 +0530

    setuped geolocator

--- lib/practice/apiCalling/weather_api.dart
+++ lib/practice/apiCalling/weather_api.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
 class WeatherApi extends StatefulWidget {
   const WeatherApi({super.key});
 
@@ -7,6 +9,12 @@ class WeatherApi extends StatefulWidget {
 }
 
 class _WeatherApiState extends State<WeatherApi> {
+  @override
+  void initState() {
+    super.initState();
+    fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -30,5 +38,34 @@ class _WeatherApiState extends State<WeatherApi> {
         ),
       ),
     );
+  }
+
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future fetchWeather() async {
+    final position = await getCurrentLocation();
+    print(position.latitude);
+    print(position.longitude);
   }
 }
